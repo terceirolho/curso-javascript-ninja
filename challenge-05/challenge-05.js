64b02c129b20e1ea31d8a64c16d6bdcbcab37379
terceirolho <64038338+terceirolho@users.noreply.github.com>
2020-07-13 14:31:11 -0300

    Update challenge-05.js

--- challenge-05/challenge-05.js
+++ challenge-05/challenge-05.js
@@ -2,17 +2,25 @@
 Crie uma variável qualquer, que receba um array com alguns valores aleatórios
 - ao menos 5 - (fica por sua conta os valores do array).
 */
-// ?
+// 
+
+var objeto = ['sol', 10, true, null, function (){}];
+
 
 /*
 Crie uma função que receba um array como parâmetro, e retorne esse array.
 */
-// ?
+// 
+function myFunction (arr){
+return arr;
+}
 
 /*
 Imprima o segundo índice do array retornado pela função criada acima.
 */
-// ?
+// 
+var arr 
+myFunction(2);
 
 /*
 Crie uma função que receba dois parâmetros: o primeiro, um array de valores; e o
@@ -20,18 +28,23 @@ segundo, um número. A função deve retornar o valor de um índice do array que
 no primeiro parâmetro. O índice usado para retornar o valor, deve ser o número passado no
 segundo parâmetro.
 */
-// ?
+//
+function MyFunction2 (array, indice){
+return array [indice]
+}
 
 /*
 Declare uma variável que recebe um array com 5 valores, de tipos diferentes.
 */
-// ?
+// var novaVar = [10, 'sol', null, true, [12,3];
+
 
 /*
 Invoque a função criada acima, fazendo-a retornar todos os valores do último
 array criado.
 */
-// ?
+// 
+nao sei
 
 /*
 Crie uma função chamada `book`, que recebe um parâmetro, que será o nome do
@@ -47,30 +60,44 @@ propriedades:
 - Se o parâmetro não for passado, a função deve retornar o objeto com todos
 os livros.
 */
-// ?
+// 
+
+function book (nomelivro) {
+  var caractLivro = {
+   'As aventuras de YU' : {
+quantidadeDePaginas : 113, autor : 'YU CHAN', editora : 'BABUM'
+  },
+    'Pulsar uma visão cosmica' : {
+      quantidadeDePaginas : 220, autor : 'Adoux', editora: 'Live'
+    },
+    'Ventiladores existem' : {
+    quantidadeDePaginas : 320, autor : 'Ventilador', editora: 'Refresca'
+  }
+ }
 
 /*
 Usando a função criada acima, imprima o objeto com todos os livros.
 */
-// ?
-
-/*
+// 
+  nao sei
+  
+  /*
 Ainda com a função acima, imprima a quantidade de páginas de um livro qualquer,
 usando a frase:
 "O livro [NOME_DO_LIVRO] tem [X] páginas!"
 */
-// ?
+// nao sei
 
 /*
 Ainda com a função acima, imprima o nome do autor de um livro qualquer, usando
 a frase:
 "O autor do livro [NOME_DO_LIVRO] é [AUTOR]."
 */
-// ?
+// nao sei
 
 /*
 Ainda com a função acima, imprima o nome da editora de um livro qualquer, usando
 a frase:
 "O livro [NOME_DO_LIVRO] foi publicado pela editora [NOME_DA_EDITORA]."
 */
-// ?
+// nao sei
