b3ca2d3f2a717e15f11a2c241156df37b5bbaa79
terceirolho <64038338+terceirolho@users.noreply.github.com>
2020-08-18 18:01:34 -0300

    Update challenge-08.js
    ainda falta responder.

--- challenge-08/challenge-08.js
+++ challenge-08/challenge-08.js
@@ -3,38 +3,57 @@ Declare uma variável chamada `sum` e atribua a ela uma função chamada
 `calculateSum`. A função deve receber dois parâmetros e retornar a soma
 desses parâmetros.
 */
-// ?
+// 
+var sum = function calculateSum (x, y){
+return x+y;
+ }
+
 
 /*
 Invoque a função criada acima, passando dois números que serão somados, e mostre
 o resultado no console, com a frase:
 "A soma de [VALOR 1] e [VALOR2] é igual a [RESULTADO]."
 */
-// ?
+// 
+
+var valor1 = 4;
+var valor2 = 8;
+console.log (' A soma de ' valor1 ' e ' + valor2 ' é igual a ' + sum (valor1, valor2) + '.');
+
 
 /*
 Mostre no console o nome da função criada acima, com a frase:
 "O nome da função que faz a soma é [NOME DA FUNÇÃO]."
 */
-// ?
+// 
+
+console.log ('O nome da função que faz a soma é ' + sum.name + ' . ');
 
 /*
 Crie uma função literal chamada `showName`. Essa função deve retornar o
 seu nome.
 */
-// ?
+// 
+
+function showName (){
+return 'LETICIA';
+}
 
 /*
 Declare uma variável chamada `varShowName` que recebe a função criada acima.
 */
-// ?
+// 
+
+var varShowName = showName;
 
 /*
 Usando a variável criada acima, mostre no console o nome e o retorno da função
 atribuída a ela, com a seguinte frase:
 "A função [NOME DA FUNÇÃO] retorna [RETORNO DA FUNÇÃO]."
 */
-// ?
+// 
+
+console.log ('A função ' + showName.name + ' retorna ' + varShowName() + '.');
 
 /*
 Crie uma função literal chamada `calculator`, que funcione assim:
@@ -48,7 +67,42 @@ vai efetuar. Será uma string com os valores `+`, `-`, `*`, `/` ou `%`;
   - Se o operador não for válido, retornar a frase:
   "Operação inválida."
 */
-// ?
+// 
+
+function calculator (operacao){
+return function (num1, num2){
+
+var result;
+switch (operacao) {
+
+case '+':
+        result = num1 + num2;
+        break;
+
+  case '-' : 
+          result = num1 - num2;
+    break;
+
+  case ' * ' :
+result = num1 * num2;
+break;
+
+  case '/' :
+result = num1 / num2;
+    break;
+
+  case ' % ' :
+    result = num1 % num2; 
+break;
+    
+    default : 
+return 'Operação inválida';
+}
+return 'Resultado da operação: ' + num1 + ' ' + operacao + ' ' + num2 + ' = ' + result + '.'
+   }
+}
+
+
 
 /*
 Declare uma variável chamada `sum`, que receberá a função acima, passando como
