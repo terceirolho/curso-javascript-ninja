13247baae8db938449597f2b0f2c8d944f71f149
terceirolho <64038338+terceirolho@users.noreply.github.com>
2020-08-22 11:03:18 -0300

    Update challenge-09.js
    vou terminar ainda

--- challenge-09/challenge-09.js
+++ challenge-09/challenge-09.js
@@ -3,6 +3,8 @@ Crie uma IIFE que envolva todo o código desse arquivo. Faça também a
 indentação correta do código, para ficar dentro da IIFE.
 */
 
+(function(){
+
 /*
 Analise as funções abaixo (`myFunction`, `myFunction2` e `myFunction3`, e
 ajuste o posicionamento das variáveis e funções internas, para que os códigos
@@ -10,11 +12,12 @@ dentro de `console.log` que estão retornando `undefined` retornem o valor
 correto da variável ou função chamada.
 */
 function myFunction() {
+  var number1 = 10;
+  var number2 = 20;
+
     console.log( 'Na função `myFunction`, o primeiro número é', number1 );
     console.log( 'Na função `myFunction`, o segundo número é', number2 );
-    var number1 = 10;
-    return number1 + number2;
-    var number2 = 20;
+ return number1 + number2;
 }
 myFunction();
 
@@ -22,13 +25,13 @@ myFunction();
     myFunction2();
 */
 function myFunction2() {
-    console.log( 'A soma de 10 e 20 é igual a', sum ? sum() : undefined );
-    var sum = function sum() {
-        return number1 + number2;
-    };
-    var number1 = 10;
+     var number1 = 10;
     var number2 = 20;
-    return sum();
+var sum = function sum() {
+return number1 + number2;
+   };
+console.log( 'A soma de 10 e 20 é igual a', sum ? sum() : undefined );
+  return sum();
 }
 myFunction2();
 
@@ -36,14 +39,14 @@ myFunction2();
     myFunction3();
 */
 function myFunction3() {
-    console.log( 'A soma de 40 e 50 é igual a', sum() );
-    var number2 = 50;
-    console.log( 'Na função myFunction3, number1 é igual a', number1 );
-    var number1 = 40;
-    return sum();
-    function sum() {
-        return number1 + number2;
-    };
+function sum() {
+return number1 + number2;
+}
+var number1 = 40;
+var number2 = 50;
+ console.log( 'A soma de 40 e 50 é igual a', sum() );
+ console.log( 'Na função myFunction3, number1 é igual a', number1 );
+ return sum();
 }
 myFunction3();
 
